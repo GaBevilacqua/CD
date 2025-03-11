6d867f928331968b8a08b4a36bd44c9eb00098cc
GaBevilacqua <gabriel.bevilacqua.2002@gmail.com>
2025-03-10 21:27:48 -0300

    21-27

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
 df = pd.read_csv("/home/bevilacqua/.cache/kagglehub/datasets/gustavomartino/brasileirao/versions/2/CampBras.csv")
+
+def mostrar_jogos_santos(df):
+    # Filtra apenas os jogos em que o Santos é o time da casa e mais que 5 gols
+    santos_em_casa = df[(df["hometeam"]=="Santos") & (df["goalsht"]>5)]
+    # Exibe os jogos
+    print(santos_em_casa)
+
+def mostrar_jogos_santosxcorinthians(df):
+    # Filtra apenas os jogos em que o Santos é o time da casa e mais que 5 gols
+    santosxcorinthians = df[((df["hometeam"] == "Santos") & (df["visitingteam"] == "Corinthians")) |
+                                  ((df["hometeam"] == "Corinthians") & (df["visitingteam"] == "Santos"))]
+    # Exibe os jogos
+    print(santosxcorinthians)
 
 #Primeiras Linhas
 #print(df.head())
@@ -11,6 +28,9 @@ df = pd.read_csv("/home/bevilacqua/.cache/kagglehub/datasets/gustavomartino/bras
 #print(df.describe())
 
 #Array das colunas
-#print(df.columns)
+#print(df.columns)# Contagem dos jogos em casa por time (hometeam)
 
-df.groupby("hometeam")["goalsht"].sum().sort_values(ascending=False)
+
+
+#mostrar_jogos_santos(df)
+mostrar_jogos_santosxcorinthians(df)
