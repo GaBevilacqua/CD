bde5fd3bf0eadbdbff3245467385af965de634ea
GaBevilacqua <gabriel.bevilacqua.2002@gmail.com>
2025-03-13 15:13:58 -0300

    13-3 15h

--- test.py
+++ test.py
@@ -8,66 +8,41 @@ df = pd.read_csv("/home/bevilacqua/.cache/kagglehub/datasets/gustavomartino/bras
 def mostrar_jogos_santos(df):
     # Filtra apenas os jogos em que o Santos é o time da casa e mais que 5 gols
     santos_em_casa = df[(df["hometeam"]=="Santos") & (df["goalsht"]>5)]
-    # Exibe os jogos
     print(santos_em_casa)
 
 def mostrar_jogos_santosxcorinthians(df):
     # Filtra apenas os jogos entre Corinthians e Santo
     santosxcorinthians = df[((df["hometeam"] == "Santos") & (df["visitingteam"] == "Corinthians")) |
                                   ((df["hometeam"] == "Corinthians") & (df["visitingteam"] == "Santos"))]
-    # Exibe os jogos
     print(santosxcorinthians)
 
 def vitoria_santos(df):
     # Filtra as vitórias
     santosvictory = df[((df["hometeam"] == "Santos") & (df["goalsht"] > df["goalsvt"])) | 
                         ((df["visitingteam"] == "Santos") & (df["goalsvt"] > df["goalsht"]))]
-    # Número de vitórias
     vitoria_count = len(santosvictory)
-    
-    # Filtra todos os jogos do Santos
     jogos_santos = df[(df["hometeam"] == "Santos") | (df["visitingteam"] == "Santos")]
     total_jogos = len(jogos_santos)
-    
-    # Cálculo da porcentagem de vitórias
     porcentagem_vitoria = (vitoria_count / total_jogos) * 100
-    
-    # Exibe os resultados
     print(f"Vitórias: {vitoria_count} ({porcentagem_vitoria:.2f}%)")
     print(santosvictory)
 
 def derrota_santos(df):
-    # Filtra as derrotas
     santosdefeat = df[((df["hometeam"] == "Santos") & (df["goalsht"] < df["goalsvt"])) | 
                        ((df["visitingteam"] == "Santos") & (df["goalsvt"] < df["goalsht"]))]
-    # Número de derrotas
     derrota_count = len(santosdefeat)
-    
-    # Filtra todos os jogos do Santos
     jogos_santos = df[(df["hometeam"] == "Santos") | (df["visitingteam"] == "Santos")]
     total_jogos = len(jogos_santos)
-    
-    # Cálculo da porcentagem de derrotas
     porcentagem_derrota = (derrota_count / total_jogos) * 100
-    
-    # Exibe os resultados
     print(f"Derrotas: {derrota_count} ({porcentagem_derrota:.2f}%)")
 
 def empate_santos(df):
-    # Filtra os empates
     santosigual = df[((df["hometeam"] == "Santos") & (df["goalsht"] == df["goalsvt"])) | 
                       ((df["visitingteam"] == "Santos") & (df["goalsvt"] == df["goalsht"]))]
-    # Número de empates
     empate_count = len(santosigual)
-    
-    # Filtra todos os jogos do Santos
     jogos_santos = df[(df["hometeam"] == "Santos") | (df["visitingteam"] == "Santos")]
     total_jogos = len(jogos_santos)
-    
-    # Cálculo da porcentagem de empates
     porcentagem_empate = (empate_count / total_jogos) * 100
-    
-    # Exibe os resultados
     print(f"Empates: {empate_count} ({porcentagem_empate:.2f}%)")
     print(total_jogos)
 
@@ -86,6 +61,40 @@ def jogostime(time, temporada):
 def jogostimes(time, time2):
     aux = df[(((df["hometeam"] == time) | (df["visitingteam"] == time)) & ((df["hometeam"] == time2) | (df["visitingteam"] == time2)))]
     print(aux)
+
+
+def mediaGolsCasa(time, temporada):
+    jogos = df[((df["hometeam"] == time) & (df["season"] == temporada))]
+    mediaGols = round(jogos["goalsht"].mean(),3)
+    print(mediaGols)
+
+def mediaGolsTemporada(time, temporada):
+    jogosH = df[((df["hometeam"] == time) & (df["season"] == temporada))]
+    jogosV = df[((df["visitingteam"] == time) & (df["season"] == temporada))]
+    mediaGolsH = round(jogosH["goalsht"].mean(),3)
+    mediaGolsV = round(jogosV["goalsvt"].mean(),3)
+    mediaGolsGeral = round((mediaGolsH+mediaGolsV) / 2, 3)
+    print("Médias de Gols em Casa: ",mediaGolsH)
+    print("Médias de Gols Fora de Casa: ",mediaGolsV)
+    print("Médias de Gols na Temporada por jogo: ",mediaGolsGeral)
+
+def mediaGolsTemporada(temporada):
+    
+    aux = df[(df["season"] == temporada)]
+    goalsH = aux["goalsht"].sum()
+    goalsV = aux["goalsvt"].sum()
+    goalsG = (goalsH+goalsV)
+    goalsGR = goalsG/38
+    goalsGRT = goalsGR/10
+
+    print("Total de Gols de equipes da Casa: ", goalsV)
+    print("Total de Gols de equipes Visitantes: ", goalsV)
+    print("Total de Gols Geral: ", goalsG)
+    print("Média de Gols por rodada: ", goalsGR)
+    print("Méida de Gols por jogo: ", goalsGRT)
+
+
+
 
 #Primeiras Linhas
 #print(df.head())
@@ -108,5 +117,7 @@ def jogostimes(time, time2):
 #empate_santos(df)
 #diadejogo(5,8,2018)
 #rodada(38,2015)
-jogostime("Grêmio", 2015)
-jogostimes("Cruzeiro", "Grêmio")
+#jogostime("Grêmio", 2015)
+#jogostimes("Cruzeiro", "Grêmio")
+#mediaGolsTemporada("Grêmio", 2019)
+mediaGolsTemporada(2019)
